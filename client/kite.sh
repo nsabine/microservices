532cd5a78ea3fba2df10bef0b8dfe28b41ed2aeb
Nick sabine <nsabine@redhat.com>
2016-03-18 16:14:44 -0400

    trying to get service discovery working

--- client/kite.sh
+++ client/kite.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export KONTROL_KONTROL_URL="http://${MICROSERVICES_SERVICE_HOST}:6000/kite"
+export KONTROL_URL="http://${MICROSERVICES_SERVICE_HOST}:6000/kite"
 export KITE_USERNAME="openshift"
 export KITE_ENVIRONMENT="openshift"
 
