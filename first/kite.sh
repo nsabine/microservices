68fe85fafe6cc629ce191cbe857cd07551ec904b
Nick sabine <nsabine@redhat.com>
2016-03-18 15:18:12 -0400

    Dockerfile for service

---
+++ first/kite.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export KONTROL_CERTS=$KITE_HOME/certs
+export KONTROL_PORT=6000
+export KONTROL_USERNAME="openshift"
+export KONTROL_STORAGE="etcd"
+export KONTROL_MACHINES="${ETCD_SERVICE_HOST}:4001"
+export KONTROL_KONTROLURL="http://${MICROSERVICES_SERVICE_HOST}:6000/kite"
+export KONTROL_PUBLICKEYFILE="$KONTROL_CERTS/key_pub.pem"
+export KONTROL_PRIVATEKEYFILE="$KONTROL_CERTS/key.pem"
+
+mkdir -p $KITE_HOME
+mkdir -p $KONTROL_CERTS
+
+
+[ -f $KONTROL_PRIVATEKEYFILE ] || openssl genrsa -out $KONTROL_PRIVATEKEYFILE 2048
+[ -f $KONTROL_PUBLICKEYFILE ] || openssl rsa -in $KONTROL_PRIVATEKEYFILE -pubout > $KONTROL_PUBLICKEYFILE
+
+env
+
+exec /go/bin/first $@
+
