152af148297f282575de2feb584dd27d52fca06e
Nick sabine <nsabine@redhat.com>
2016-03-18 23:02:20 -0400

    fix kite registration

--- square/square.go
+++ square/square.go
@@ -9,11 +9,15 @@ import (
 func main() {
 	k := kite.New("square", "1.0.0")
 	k.Config.Port = 6001
+        k.Config.KontrolURL=os.Getenv("KITE_KONTROL_URL")
+        k.Config.Username=os.Getenv("KITE_USERNAME")
+        k.Config.Environment=os.Getenv("KITE_ENVIRONMENT")
+
 	k.HandleFunc("square", func(r *kite.Request) (interface{}, error) {
 		a := r.Args.One().MustFloat64()
 		return a * a, nil
 	})
 
-	k.Register(&url.URL{Scheme: "http", Host: os.Getenv("KONTROL_SERVICE_HOST")+":6000/kite"})
+	k.Register(&url.URL{Scheme: "http", Host: os.Getenv("SQUARE_SERVICE_HOST")+":6001/kite"})
 	k.Run()
 }
