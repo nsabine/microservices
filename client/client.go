80a07c70a5339d7bee4373a772f1a6a9ee88f125
Nick sabine <nsabine@redhat.com>
2016-03-19 01:17:01 -0400

    hardcoded square service url

--- client/client.go
+++ client/client.go
@@ -9,28 +9,16 @@ import (
 
 func main() {
 	k := kite.New("client", "1.0.0")
-	k.Config.KontrolURL=os.Getenv("KITE_KONTROL_URL")
-	k.Config.Username=os.Getenv("KITE_USERNAME")
         k.Config.DisableAuthentication = true
-	
+
 	fmt.Println(k.Config)
 
-	// search a kite that has the same username and environment as us, but the
-	// kite name should be "square"
-	kites, _ := k.GetKites(&protocol.KontrolQuery{
-		Username:    k.Config.Username,
-		Environment: k.Config.Environment,
-		Name:        "square",
-	})
-
-	// there might be several kites that matches our query
-	for _, fetchedKite := range kites {
-		fetchedKite.Dial()
-
-		response, err := fetchedKite.Tell("square", 4)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(response.MustFloat64())
+	client = k.NewClient("http://square.openshiftapps.com:6001/kite")
+	client.Dial()
+	
+	response, err := fetchedKite.Tell("square", 4)
+	if err != nil {
+		panic(err)
 	}
+	fmt.Println(response.MustFloat64())
 }
