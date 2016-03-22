629fe4d7347ffea477f1137852c57eb78a2e84ce
Nick sabine <nsabine@redhat.com>
2016-03-22 00:23:31 -0400

    adding

--- controller/main.go
+++ controller/main.go
@@ -12,6 +12,8 @@ import (
 )
 
 func main() {
+	XSize := 25
+	YSize := 25
 	fmt.Println("Starting Controller")
 	runtime.GOMAXPROCS(2)
 	wg := &sync.WaitGroup{}
@@ -37,15 +39,20 @@ func startMessaging() {
   	w, _ := nsq.NewProducer(os.Getenv("MESSAGING_SERVICE_HOST") + ":4150", config)
 
 	for {
-		err := w.Publish("tick", []byte("test"))
-		if err != nil {
-			log.Panic("Could not connect")
-		}
+		tick(w)
 		time.Sleep(time.Second * 1)
-
 	}	
 	w.Stop()
 
+}
+
+func tick(w *nsq.Producer) {
+	evaluate()	
+	err := w.Publish("tick", []byte("test"))
+	if err != nil {
+		log.Panic("Could not connect")
+	}
+	
 }
 
 func hello(r *kite.Request) (interface{}, error) {
@@ -55,3 +62,32 @@ func hello(r *kite.Request) (interface{}, error) {
 	// You can return anything as result, as long as it is JSON marshalable.
 	return nil, nil
 }
+
+func update(r *kite.Request) (interface{}, error) {
+        fmt.Println("Controller received state update")
+        return nil, nil
+}
+
+func getState(r *kite.Request) (interface{}, error) {
+        fmt.Println("Controller received state request")
+        return state, nil
+}
+
+func reset() {
+	// Allocate the top-level slice.
+	GameState := make([][]uint8, YSize) // One row per unit of y.
+	// Loop over the rows, allocating the slice for each row.
+	for i := range GameState {
+		GameState[i] = make([]uint8, XSize)
+	}
+}
+
+func evaluate() {
+	for i := range GameState {
+		for j := range GameState[i] {
+			fmt.Print(GameState[i][j])
+		}
+		fmt.Println()
+	}
+}
+
