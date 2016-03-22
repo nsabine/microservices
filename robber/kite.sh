45402a0fb4a808c9f0cc2aef2d92d548a32042df
Nick sabine <nsabine@redhat.com>
2016-03-21 22:52:43 -0400

    initial robber

---
+++ robber/kite.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+export KONTROL_CERTS=$KITE_HOME/certs
+export KONTROL_PORT=6000
+export KITE_USERNAME="openshift"
+export KITE_ENVIRONMENT="openshift"
+export KONTROL_STORAGE="etcd"
+export KONTROL_MACHINES="${ETCD_SERVICE_HOST}:4001"
+export KITE_KONTROL_URL="http://${KONTROL_SERVICE_HOST}:6000/kite"
+export KONTROL_PUBLICKEYFILE="$KONTROL_CERTS/key_pub.pem"
+export KONTROL_PRIVATEKEYFILE="$KONTROL_CERTS/key.pem"
+
+mkdir -p $KITE_HOME
+mkdir -p $KONTROL_CERTS
+
+
+[ -f $KONTROL_PRIVATEKEYFILE ] || openssl genrsa -out $KONTROL_PRIVATEKEYFILE 2048
+[ -f $KONTROL_PUBLICKEYFILE ] || openssl rsa -in $KONTROL_PRIVATEKEYFILE -pubout > $KONTROL_PUBLICKEYFILE
+
+env
+
+echo "Starting Robber Microservice"
+exec /go/bin/robber $@
+
---
+++ robber/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/koding/kite"
+	"fmt"
+	"os"
+	"github.com/bitly/go-nsq"
+)
+
+func main() {
+	k := kite.New("robber", "1.0.0")
+	k.Config.Port = 6002
+	k.Config.DisableAuthentication = true
+	k.HandleFunc("robber", robber)
+	
+	fmt.Println("Robber staring kite")
+	k.Run()
+
+	
+	fmt.Println("Robber configuring NSQ")
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	config := nsq.NewConfig()
+	q, _ := nsq.NewConsumer("tick", "ch", config)
+	q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		log.Printf("Got a message: %v", message)
+		wg.Done()
+		return nil
+	}))
+
+	err := q.ConnectToNSQD(os.Getenv("MESSAGING_SERVICE_HOST" + ":4150")
+	if err != nil {
+		log.Panic("Could not connect")
+	}
+	fmt.Println("Robber starting NSQ")
+	wg.Wait()
+
+
+}
+
+func hello(r *kite.Request) (interface{}, error) {
+
+	fmt.Println("Robber got hello")
+
+	// You can return anything as result, as long as it is JSON marshalable.
+	return nil
+}
